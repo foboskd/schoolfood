99b2505e208d26384f7310b87c99b87fc2d5d420
Kirill Konovalov <DestSH@yandex.ru>
2020-11-24 13:03:21 +0300

    add : request for creating school

--- app/Http/Controllers/Admin/SchoolController.php
+++ app/Http/Controllers/Admin/SchoolController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateSchoolRequest;
 use Illuminate\Http\Request;
 
 class SchoolController extends Controller {
 
     public function create() {
+
+    }
+
+    public function store(CreateSchoolRequest $request) {
 
     }
 
---
+++ app/Http/Requests/CreateSchoolRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateSchoolRequest extends FormRequest {
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize() {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() {
+        return [
+            'title' => 'required|string|between:3,255|unique:App\Models\School,title',
+            'address' => 'required|string|between:3,255',
+        ];
+    }
+}
