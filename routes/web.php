0e40f60c827615e566ff72c26a9a9344cdd2f162
Kirill Konovalov <DestSH@yandex.ru>
2020-11-24 05:53:50 +0300

    add : default SPA view and site controller

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    Route::get('/{any}', SiteController::class)->where('any', '.*');
 });
