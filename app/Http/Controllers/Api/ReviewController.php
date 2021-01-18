adff34b2c1a9b96cb7d1c9e9b2ebb1d496d02380
Kirill Konovalov <DestSH@yandex.ru>
2021-01-18 18:26:16 +0300

    add : review log save

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Builders\ReviewBuilder;
+use App\Builders\ReviewLogBuilder;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateReviewRequest;
 use App\Repositories\ReviewRepository;
@@ -13,14 +14,23 @@ class ReviewController extends Controller {
 
     protected $builder;
 
+    protected $log_builder;
+
     protected $repository;
 
     public function __construct() {
         $this->builder = new ReviewBuilder();
+        $this->log_builder = new ReviewLogBuilder();
         $this->repository = new ReviewRepository();
     }
 
     public function store(CreateReviewRequest $request) {
+        $this->log_builder
+            ->createEmpty()
+            ->setFingerprint($request->uuid)
+            ->setLongitude($request->longitude)
+            ->setLatitude($request->latitude);
+
         $last_review = $this->repository->getLastByFingerprint($request->fingerprint);
 
         if (!$last_review) {
@@ -38,9 +48,14 @@ class ReviewController extends Controller {
 
             $this->builder->save();
 
+            $this->log_builder
+                ->setReviewUuid($this->builder->getModel()->uuid)
+                ->save();
+
             return response('', 200);
         } else {
-            return response('', 400);
+            $this->log_builder->save();
+            return response('Вы уже голосовали сегодня', 400);
         }
     }
 }
